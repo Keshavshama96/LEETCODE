025284d15848fa4f71b68de64ef7e3c172596ff2
keshav-Shama-07 <keshavshama1@gmail.com>
2026-06-25 00:49:23 +0530

    Time: 52 ms (22.92%), Space: 84.9 MB (33.64%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,17 +6,17 @@ public:
         int j=0;
         int maxfruits=0;
         while(j<fruits.size()){
-            mp[fruits[j]]++;//frequency increasing
+            mp[fruits[j]]++;//current fruit ki frequency increase
             
              if(mp.size()<=2){//mtlb 2 fruits (valid)
-             maxfruits=max(maxfruits,j-i+1);//updation of the maxlen
+             maxfruits=max(maxfruits,j-i+1);//updation of the maxfruits
              j++;
              }
              //window shrinking 
-             else if(mp.size()>2){//mtlb invalid
+             else if(mp.size()>2){//mtlb invalid window
               while(mp.size()>2){
                  mp[fruits[i]]--;//kam kro frequency left se
-                 if(mp[fruits[i]]==0){//mtlb left ko htana hai 
+                 if(mp[fruits[i]]==0){//// fruit completely removed
                     mp.erase(fruits[i]);//htado
                  }
                  i++;//shrink
