b5b28731fcc0210200a6e5bf4a293eacd989313d
keshav-Shama-07 <keshavshama1@gmail.com>
2026-06-24 15:54:04 +0530

    Time: 11 ms (48.71%), Space: 12.5 MB (38.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,18 +7,18 @@ public:
        int maxlen=0;
         while(j<s.size()){
          mp[s[j]]++;
-             
+             // agr mp ka size == window size ...mtlb valid hai 
            if(mp.size()==j-i+1){
               maxlen=max(maxlen,j-i+1);
               j++;
-           }
+           }//winodw bdi hai mtlb koi to duplicate aagya ab 
            else if(mp.size()<j-i+1){
-                while(mp.size()<j-i+1){
-                    mp[s[i]]--;
-                    if(mp[s[i]] == 0) {
-                        mp.erase(s[i]);
+                while(mp.size()<j-i+1){//jab tak bdi hai window size
+                    mp[s[i]]--;//left waale ki frquency kam kro
+                    if(mp[s[i]] == 0) {//agar frequency 0 ho gayi
+                        mp.erase(s[i]);//map se remove karo
                     }
-                    i++;
+                    i++;//shrink window
                 }
               j++;
            }
