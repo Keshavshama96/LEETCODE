00993c4931774c34060f0fe90e926715932a8f61
keshav-Shama-07 <keshavshama1@gmail.com>
2026-06-27 16:33:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (11.56%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,46 @@
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+        
+//         int minlen=INT_MAX;
+//         for(int i=0;i<nums.size();i++){
+//                int sum=0;
+//             for(int j=i;j<nums.size();j++){
+//                sum=sum+nums[j];
+//                if(sum>=target){
+//                 minlen=min(minlen,j-i+1);
+//                }
+//             }
+//         }
+//         return (minlen == INT_MAX) ? 0 : minlen;//"Agar minlen abhi bhi INT_MAX hai, iska matlab koi bhi subarray target tak nahi pahunchi. Isliye 0 return kar do."
+//     }
+// };
+
+
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int i=0;
+        int j=0;
+        int minlen=INT_MAX;
+        int sum=0;
+       while(j<nums.size()){
+        sum+=nums[j];
+
+        if(sum<target){
+            j++;
+        }
+        else if(sum>=target){//valid
+           while(sum>=target){
+            minlen=min(minlen,j-i+1);
+            sum-=nums[i];
+            i++;
+           }
+           j++;
+        }
+        
+       }
+        return (minlen == INT_MAX) ? 0 : minlen;
+    }
+};
