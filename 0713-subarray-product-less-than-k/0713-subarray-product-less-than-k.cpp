a432c14f1010b73118a7b3cd952c6016999b00fc
keshav-Shama-07 <keshavshama1@gmail.com>
2026-06-28 17:46:38 +0530

    Time: 0 ms (100%), Space: 65.3 MB (34.75%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
          
-         int i=0;
-         int j=0;
-         long long count=0;
-         long long p=1;
-         while(j<nums.size()){
+        int i = 0;
+        int j = 0;
+        long long count = 0;
+        long long p = 1;
+
+        while(j < nums.size()){
+
             if(k <= 1) return 0;
-            p*=nums[j];
-            if(p<k){//valid
-                count+=j-i+1;
-                j++;
-            }else if(p>=k){//invalid
-                while(p>=k){//making valid by shrinking
-                    p=p/nums[i];
-                    i++;
-                }
-                if(p<k){
-                     count+=j-i+1;
-                    }
+            p *= nums[j];
+            if(p < k){ 
+             // valid window: all subarrays ending at j are valid
+             count += j - i + 1; //jitna subarry hai vo sbh valid hai yha pe
                 j++;
             }
-         }
-         return count ;
+            else if(p >= k){ 
+                // invalid window: need to shrink from left
+                while(p >= k){ 
+                 // making valid by shrinking
+                 p = p / nums[i];
+                 i++;
+                }
+                //loop is ended now which means window is valid now, so we must count it before moving j(nhi to j chnge kr dega window)
+                count += j - i + 1;
+                j++;
+            }
+        }
+        return count;
     }
 };
