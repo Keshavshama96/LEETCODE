71f01bf9dde734778aa79fc2bedd1a0962e5e6ce
keshav-Shama-07 <keshavshama1@gmail.com>
2026-07-01 19:02:49 +0530

    Time: 0 ms (100%), Space: 32.7 MB (62.81%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
 
+    // This function counts subarrays with sum <= goal
     int atMost(vector<int>& nums, int goal){
 
+        // If goal is negative, no subarray possible
         if(goal < 0) return 0;
 
-        int i = 0;
-        int sum = 0;
-        int count = 0;
+        int i = 0;        // left pointer
+        int sum = 0;      // current window sum
+        int count = 0;    // total subarrays
 
+        // right pointer expands window
         for(int j = 0; j < nums.size(); j++){
 
-            sum += nums[j];
+            sum += nums[j];   // include current element
 
+            // if sum becomes invalid, shrink window
             while(sum > goal){
-                sum -= nums[i];
-                i++;
+                sum -= nums[i];  // remove left element
+                i++;              // move left pointer
             }
 
+            // all subarrays ending at j and starting from i to j are valid
             count += (j - i + 1);
         }
 
@@ -25,6 +30,9 @@ public:
     }
 
     int numSubarraysWithSum(vector<int>& nums, int goal) {
+
+        // Idea:
+        // exact = atMost(goal) - atMost(goal - 1)
 
         return atMost(nums, goal) - atMost(nums, goal - 1);
     }
